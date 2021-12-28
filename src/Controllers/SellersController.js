ecd61e5f8e23807e78ec5f9677c76567b4e3ade5
Fellipe Lorram <fellipelorram@gmail.com>
2021-12-28 09:10:49 -0300

    Changes on the returning of sellers SHOW route

--- src/Controllers/SellersController.js
+++ src/Controllers/SellersController.js
@@ -16,7 +16,7 @@ class SellersController {
     try {
       const seller = await Seller.find(req.userId, req.params.id);
       if (!seller) return res.json(null);
-      return res.json(seller.sellers);
+      return res.json(seller.sellers[0]);
     } catch (e) {
       console.log(e);
       return res.json([{
